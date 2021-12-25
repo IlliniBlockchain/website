5b6e838a4db31403ff567b0ea6d109872d0f3b7c
Zayyan Faizal <zfaizal2@illinois.edu>
2021-12-24 21:00:06 -0600

    🧑🏾‍🤝‍🧑🏾 About Us (#23)
    * init about us
    
    * delete yarn lock
    
    * Add Member components for About Us Page and CSS Structure cleanup
    
    * add social profiles and headshots
    
    * fix container sizing for footer
    
    * refactored css files and added headshots
    
    * add socialicon container
    
    * Fix imports after deleting styles folder
    
    * add danny headshot
    
    Co-authored-by: Alec Chen <alecchendev@gmail.com>

--- components/shared-styled/index.js
+++ components/shared-styled/index.js
@@ -1,2 +1,8 @@
 export { default as SectionTitle } from './SectionTitle'
 export { default as GlobalStyle } from './GlobalStyle'
+export { default as Container } from './Container'
+export { default as MemberImage } from './MemberImage'
+export { default as MemberInfo } from './MemberInfo'
+export { default as MembersContainer } from './MembersContainer'
+export { default as Nametag } from './Nametag'
+export { default as SocialIcon } from './SocialIcon'
