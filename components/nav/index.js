df9e4818258318aaba764bdab2ccef91d3f16aad
Zayyan <zayyanf@gmail.com>
2021-12-31 19:59:04 -0600

    add logo to nav bar

--- components/nav/index.js
+++ components/nav/index.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import Link from 'next/link';
-
+import Image from 'next/image'
 const Nav = styled.nav`
-padding-left: 85px;
+padding-left: 1rem;
+margin-top:10px;
 height: 80px;
 color: #fff; 
 display: flex;
@@ -15,7 +16,6 @@ z-index:1;
 
 const StyledLink = styled.a`
 padding: 2rem;
-margin-right: 75px;
 padding-top: 10px;
 padding-bottom: 10px;
   color: white;
@@ -50,24 +50,29 @@ margin-right: 100px;
 const Navbar = () => {
     return (
         <Nav>
-            <div>
-            <Link href='/' passHref>
-                <StyledLink>Home</StyledLink>
-            </Link>
-            <Link href='/about' passHref>
-                <StyledLink>About Us</StyledLink>
-            </Link>
-            <Link href='/projects' passHref>
-                <StyledLink>Projects</StyledLink>
-            </Link>
-            <Link href='/blog' passHref>
-                <StyledLink>Blogs</StyledLink>
-            </Link>
+            <div style={{display:"flex", marginLeft:'40px', alignItems:"center"}}>
+              <Image src="/blocks_transparent.png" width="50px" height="50px" layout="fixed"/>
+              <div style={{fontSize:"1.5rem", padding:"1rem"}}> ILLINI BLOCKCHAIN </div>
             </div>
             <div>
-            <Link href='/contact' passHref>
-                <StyledLinkTwo> Contact Us</StyledLinkTwo>
-            </Link>
+
+              <Link href='/' passHref>
+                  <StyledLink>Home</StyledLink>
+              </Link>
+              <Link href='/about' passHref>
+                  <StyledLink>About Us</StyledLink>
+              </Link>
+              <Link href='/projects' passHref>
+                  <StyledLink>Projects</StyledLink>
+              </Link>
+              <Link href='/blog' passHref>
+                  <StyledLink>Blogs</StyledLink>
+              </Link>
+            </div>
+            <div>
+              <Link href='/contact' passHref>
+                  <StyledLinkTwo> Contact Us</StyledLinkTwo>
+              </Link>
             </div>
         </Nav>
     )
