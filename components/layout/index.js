595428d578b9103dde69f9beba7dff78349a7691
Zayyan <zayyanf@gmail.com>
2022-01-01 23:19:07 -0600

    fix linting for deploy

--- components/layout/index.js
+++ components/layout/index.js
@@ -7,7 +7,7 @@ export default function Layout ({children}) {
     <> 
         <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"/> 
         </Head>
         <Navbar /> 
