49846bc05f51542788d6c0b9d473fc7b5bf64237
Zayyan <zayyanf@gmail.com>
2022-01-17 11:50:23 -0600

    fix link for deploy

--- components/home/ApplyBanner/index.js
+++ components/home/ApplyBanner/index.js
@@ -9,7 +9,7 @@ const ApplyBanner = () => {
         <ApplyHeader>Looking to join the team?</ApplyHeader>
         <ApplySubheader>Learn more about how you can join the team and the roles we're looking for!</ApplySubheader>
     </ApplyContainer>
-    <Link href="https://www.notion.so/illiniblockchain/Spring-2022-Recruitment-ff8e4c8165c54840bda5a77e4e95edbe" passHref>
+    <Link href='https://www.notion.so/illiniblockchain/Spring-2022-Recruitment-ff8e4c8165c54840bda5a77e4e95edbe' passHref>
       <ApplyButton>Apply</ApplyButton>
     </Link>
   </ApplyBackground>
