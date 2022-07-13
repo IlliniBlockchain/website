b4be5878e6a9b273d51be53394c6f9477fb35715
Alec Chen <alecchendev@gmail.com>
2022-07-12 21:14:57 -0400

    Update to new logo

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,71 +1,138 @@
-import styled from "styled-components"
-import { AiOutlineTwitter, AiFillMediumSquare, AiFillGithub } from 'react-icons/ai'
-import { FaDiscord } from 'react-icons/fa'
-import { SiNotion } from 'react-icons/si'
-import Image from 'next/image'
+import styled from "styled-components";
 import {
-	FooterSection,
-	FooterContainer,
-	FooterLeftContainer,
-	FooterLogoContainer,
-	FooterImageContainer,
-	FooterLinksContainer,
-	FooterLinksSectionContainer,
-	FooterSocialsContainer
-} from './FooterStyles';
-import NewsletterBanner from "../home/NewsletterBanner"
-
+  AiOutlineTwitter,
+  AiFillMediumSquare,
+  AiFillGithub,
+} from "react-icons/ai";
+import { FaDiscord } from "react-icons/fa";
+import { SiNotion } from "react-icons/si";
+import Image from "next/image";
+import {
+  FooterSection,
+  FooterContainer,
+  FooterLeftContainer,
+  FooterLogoContainer,
+  FooterImageContainer,
+  FooterLinksContainer,
+  FooterLinksSectionContainer,
+  FooterSocialsContainer,
+} from "./FooterStyles";
+import NewsletterBanner from "../home/NewsletterBanner";
 
 const Footer = () => {
-	return (
-		<FooterSection>
-			<FooterContainer>
-				<FooterLeftContainer>
+  return (
+    <FooterSection>
+      <FooterContainer>
+        <FooterLeftContainer>
+          <FooterLogoContainer>
+            <FooterImageContainer>
+              <Image
+                src="/IlliniBlockchainLogoDarkMode.png"
+                alt="Illini Blockchain Logo"
+                layout="responsive"
+                width={100}
+                height={100}
+              />
+            </FooterImageContainer>
+            <h3>Illini Blockchain</h3>
+          </FooterLogoContainer>
+        </FooterLeftContainer>
 
-					<FooterLogoContainer>
-						<FooterImageContainer>
-							<Image src="/illiniBlockchainLogo.png" alt="Illini Blockchain Logo" layout="responsive" width={100} height={100}/>
-						</FooterImageContainer>
-						<h3>Illini Blockchain</h3>
-					</FooterLogoContainer>
+        <FooterLinksContainer>
+          <FooterLinksSectionContainer>
+            <h4>Resources</h4>
+            <a
+              href="https://illiniblockchain.notion.site/bcd75e1fbede4d81a723eb26d81aed16?v=50ec9fbb542b4d08bc817a03d593c23f"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Resources Database
+            </a>
+            <a
+              href="https://illiniblockchain.notion.site/0dcdd7753e2d4906aac1cd3597112d82?v=43b8da323cae498388e9e8e716a4af37"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Crypto Thinkers
+            </a>
+            <a
+              href="https://illiniblockchain.notion.site/Down-the-Crypto-Rabbithole-40a6ca69fa394f7eb87b27f33772b68c"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Down the Rabbit Hole
+            </a>
+          </FooterLinksSectionContainer>
+          <FooterLinksSectionContainer>
+            <h4>Projects</h4>
+            <a
+              href="https://illini-mint.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              NFT Mint
+            </a>
+            <a>IlliniDAO</a>
+          </FooterLinksSectionContainer>
+          <FooterLinksSectionContainer>
+            <h4>Blog</h4>
+            <a
+              href="https://medium.com/@illiniblockchain/bitcoin-under-the-hood-how-does-it-actually-work-a6a64371d1e9"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Bitcoin
+            </a>
+            <a
+              href="https://medium.com/@illiniblockchain/ethereum-under-the-hood-a-decentralized-computer-bb4130f1804"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Ethereum
+            </a>
+          </FooterLinksSectionContainer>
+        </FooterLinksContainer>
 
+        <FooterSocialsContainer>
+          <a
+            href="https://twitter.com/ILL_Blockchain"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineTwitter />
+          </a>
+          <a
+            href="https://medium.com/@illiniblockchain"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillMediumSquare />
+          </a>
+          <a
+            href="https://discord.gg/42FxuQXc"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaDiscord />
+          </a>
+          <a
+            href="https://illiniblockchain.notion.site/Illini-Blockchain-Public-ac1f93f3ad984e3fa00ce6a42c69b335"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <SiNotion />
+          </a>
+          <a
+            href="https://github.com/IlliniBlockchain"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillGithub />
+          </a>
+        </FooterSocialsContainer>
+      </FooterContainer>
+    </FooterSection>
+  );
+};
 
-				</FooterLeftContainer>
-
-
-				<FooterLinksContainer>
-					<FooterLinksSectionContainer>
-						<h4>Resources</h4>
-						<a href="https://illiniblockchain.notion.site/bcd75e1fbede4d81a723eb26d81aed16?v=50ec9fbb542b4d08bc817a03d593c23f" target="_blank" rel="noreferrer">Resources Database</a>
-						<a href="https://illiniblockchain.notion.site/0dcdd7753e2d4906aac1cd3597112d82?v=43b8da323cae498388e9e8e716a4af37" target="_blank" rel="noreferrer">Crypto Thinkers</a>
-						<a href="https://illiniblockchain.notion.site/Down-the-Crypto-Rabbithole-40a6ca69fa394f7eb87b27f33772b68c" target="_blank" rel="noreferrer">Down the Rabbit Hole</a>
-					</FooterLinksSectionContainer>
-					<FooterLinksSectionContainer>
-						<h4>Projects</h4>
-						<a href="https://illini-mint.vercel.app/" target="_blank" rel="noreferrer">NFT Mint</a>
-						<a>IlliniDAO</a>
-					</FooterLinksSectionContainer>
-					<FooterLinksSectionContainer>
-						<h4>Blog</h4>
-						<a href="https://medium.com/@illiniblockchain/bitcoin-under-the-hood-how-does-it-actually-work-a6a64371d1e9" target="_blank" rel="noreferrer">Bitcoin</a>
-						<a href="https://medium.com/@illiniblockchain/ethereum-under-the-hood-a-decentralized-computer-bb4130f1804" target="_blank" rel="noreferrer">Ethereum</a>
-					</FooterLinksSectionContainer>
-				</FooterLinksContainer>
-
-
-				<FooterSocialsContainer>
-					<a href="https://twitter.com/ILL_Blockchain" target="_blank" rel="noreferrer"><AiOutlineTwitter/></a>
-					<a href="https://medium.com/@illiniblockchain" target="_blank" rel="noreferrer"><AiFillMediumSquare/></a>
-					<a href="https://discord.gg/42FxuQXc" target="_blank" rel="noreferrer"><FaDiscord/></a>
-					<a href="https://illiniblockchain.notion.site/Illini-Blockchain-Public-ac1f93f3ad984e3fa00ce6a42c69b335" target="_blank" rel="noreferrer"><SiNotion/></a>
-					<a href="https://github.com/IlliniBlockchain" target="_blank" rel="noreferrer"><AiFillGithub/></a>
-				</FooterSocialsContainer>
-
-			</FooterContainer>
-
-		</FooterSection>
-
-	)
-}
-
-export default Footer
+export default Footer;
--- components/nav/index.js
+++ components/nav/index.js
@@ -1,78 +1,97 @@
-import Link from 'next/link';
-import Image from 'next/image'
-import { Nav, StyledLink, StyledLinkTwo, IconWrapper, LogoText, LogoLink, MobileStyledLink, MobileNav, MobileBackground } from './NavStyles';
-import { GiHamburgerMenu, GiExitDoor } from 'react-icons/gi';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router'
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Nav,
+  StyledLink,
+  StyledLinkTwo,
+  IconWrapper,
+  LogoText,
+  LogoLink,
+  MobileStyledLink,
+  MobileNav,
+  MobileBackground,
+} from "./NavStyles";
+import { GiHamburgerMenu, GiExitDoor } from "react-icons/gi";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
   const router = useRouter();
   const [nav, setNav] = useState(false);
 
   useEffect(() => {
-    setNav(false)
-  }, [router.pathname])
-  
-    return (
-        <Nav>
-            <Link href='/' passHref>
-              <LogoLink>
-                <Image src="/blocks_transparent.png" width="50px" height="50px" layout="fixed"/>
-                {nav ?
-                  <></> :
-                  <LogoText> ILLINI BLOCKCHAIN </LogoText> 
-                }
-              </LogoLink>
+    setNav(false);
+  }, [router.pathname]);
+
+  return (
+    <Nav>
+      <Link href="/" passHref>
+        <LogoLink>
+          <Image
+            src="/IlliniBlockchainLogoDarkMode.png"
+            width="50px"
+            height="50px"
+            layout="fixed"
+          />
+          {nav ? <></> : <LogoText> ILLINI BLOCKCHAIN </LogoText>}
+        </LogoLink>
+      </Link>
+
+      {nav ? (
+        <>
+          <MobileNav>
+            <Link href="/about" passHref>
+              <MobileStyledLink>About Us</MobileStyledLink>
             </Link>
-
-              { nav ? 
-                <>
-                  <MobileNav>
-                    <Link href='/about' passHref>
-                        <MobileStyledLink >About Us</MobileStyledLink>
-                    </Link>
-                    <Link href='/projects' passHref>
-                        <MobileStyledLink>Projects</MobileStyledLink>
-                    </Link>
-                    <a href='https://medium.com/illiniblockchain' target="_blank" rel="noreferrer">
-                        <MobileStyledLink>Blogs</MobileStyledLink>
-                    </a>
-                    <Link href='mailto:illiniblockchain@gmail.com' passHref>
-                        <MobileStyledLink> Contact Us</MobileStyledLink>
-                    </Link>
-                    <MobileBackground/>
-
-                  </MobileNav>
-                </>
-                :
-                <>
-                  <div>
-                    <Link href='/about' passHref>
-                        <StyledLink>About Us</StyledLink>
-                    </Link>
-                    <Link href='/projects' passHref>
-                        <StyledLink>Projects</StyledLink>
-                    </Link>
-                    <a href='https://medium.com/illiniblockchain' target="_blank" rel="noreferrer">
-                        <StyledLink>Blogs</StyledLink>
-                    </a>
-                  </div>
-                  <div>
-                    <Link href='mailto:illiniblockchain@gmail.com' passHref>
-                        <StyledLinkTwo> Contact Us</StyledLinkTwo>
-                    </Link>
-                  </div>
-                </>
-              }
-            <IconWrapper>
-              { nav ?
-                <GiExitDoor onClick={() => setNav(!nav)} style={{color:"black"}}/>
-                : 
-                <GiHamburgerMenu onClick={() => setNav(!nav)}/>
-              }
-            </IconWrapper>
-        </Nav>
-    )
-}
+            <Link href="/projects" passHref>
+              <MobileStyledLink>Projects</MobileStyledLink>
+            </Link>
+            <a
+              href="https://medium.com/illiniblockchain"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <MobileStyledLink>Blogs</MobileStyledLink>
+            </a>
+            <Link href="mailto:illiniblockchain@gmail.com" passHref>
+              <MobileStyledLink> Contact Us</MobileStyledLink>
+            </Link>
+            <MobileBackground />
+          </MobileNav>
+        </>
+      ) : (
+        <>
+          <div>
+            <Link href="/about" passHref>
+              <StyledLink>About Us</StyledLink>
+            </Link>
+            <Link href="/projects" passHref>
+              <StyledLink>Projects</StyledLink>
+            </Link>
+            <a
+              href="https://medium.com/illiniblockchain"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <StyledLink>Blogs</StyledLink>
+            </a>
+          </div>
+          <div>
+            <Link href="mailto:illiniblockchain@gmail.com" passHref>
+              <StyledLinkTwo> Contact Us</StyledLinkTwo>
+            </Link>
+          </div>
+        </>
+      )}
+      <IconWrapper>
+        {nav ? (
+          <GiExitDoor onClick={() => setNav(!nav)} style={{ color: "black" }} />
+        ) : (
+          <GiHamburgerMenu onClick={() => setNav(!nav)} />
+        )}
+      </IconWrapper>
+    </Nav>
+  );
+};
 
 export default Navbar;
