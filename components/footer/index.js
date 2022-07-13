1c8e5cc6b4e9c311872b8fe76dab917a2791753d
Alec Chen <alecchendev@gmail.com>
2022-07-12 22:12:58 -0400

    Update footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -15,6 +15,9 @@ import {
   FooterImageContainer,
   FooterLinksContainer,
   FooterLinksSectionContainer,
+  FooterBottomContainer,
+  FooterSponsorsContainer,
+  FooterSponsorImageContainer,
   FooterSocialsContainer,
 } from "./FooterStyles";
 import NewsletterBanner from "../home/NewsletterBanner";
@@ -42,29 +45,39 @@ const Footer = () => {
           <FooterLinksSectionContainer>
             <h4>Resources</h4>
             <a
-              href="https://illiniblockchain.notion.site/bcd75e1fbede4d81a723eb26d81aed16?v=50ec9fbb542b4d08bc817a03d593c23f"
+              href="https://www.notion.so/illiniblockchain/web3-Starter-Pack-66bf4bd20bca4d26958cf78af57d379d"
               target="_blank"
               rel="noreferrer"
             >
-              Resources Database
+              Web3 Starter Pack
             </a>
             <a
-              href="https://illiniblockchain.notion.site/0dcdd7753e2d4906aac1cd3597112d82?v=43b8da323cae498388e9e8e716a4af37"
+              href="https://www.notion.so/illiniblockchain/Crypto-Glossary-f3724122e76f4728a2602d011efb8c91"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Crypto Glossary
+            </a>
+            <a
+              href="https://www.notion.so/illiniblockchain/0dcdd7753e2d4906aac1cd3597112d82?v=43b8da323cae498388e9e8e716a4af37"
               target="_blank"
               rel="noreferrer"
             >
               Crypto Thinkers
             </a>
-            <a
-              href="https://illiniblockchain.notion.site/Down-the-Crypto-Rabbithole-40a6ca69fa394f7eb87b27f33772b68c"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Down the Rabbit Hole
-            </a>
           </FooterLinksSectionContainer>
           <FooterLinksSectionContainer>
             <h4>Projects</h4>
+            <a href="https://illinidao.com" target="_blank" rel="noreferrer">
+              IlliniDAO
+            </a>
+            <a
+              href="https://buoyantprotocol.xyz"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Buoyant Protocol
+            </a>
             <a
               href="https://illini-mint.vercel.app/"
               target="_blank"
@@ -72,10 +85,9 @@ const Footer = () => {
             >
               NFT Mint
             </a>
-            <a>IlliniDAO</a>
           </FooterLinksSectionContainer>
           <FooterLinksSectionContainer>
-            <h4>Blog</h4>
+            <h4>Blog Posts</h4>
             <a
               href="https://medium.com/@illiniblockchain/bitcoin-under-the-hood-how-does-it-actually-work-a6a64371d1e9"
               target="_blank"
@@ -90,46 +102,74 @@ const Footer = () => {
             >
               Ethereum
             </a>
+            <a
+              href="https://medium.com/illiniblockchain/cbdcs-the-future-of-money-aaf43d7ec17a"
+              target="_blank"
+              rel="noreferrer"
+            >
+              CBDCs
+            </a>
+            <a
+              href="https://medium.com/illiniblockchain/how-blockchain-can-make-our-next-election-more-secure-c547a3962ed4"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Secure Elections
+            </a>
           </FooterLinksSectionContainer>
         </FooterLinksContainer>
 
-        <FooterSocialsContainer>
-          <a
-            href="https://twitter.com/ILL_Blockchain"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <AiOutlineTwitter />
-          </a>
-          <a
-            href="https://medium.com/@illiniblockchain"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <AiFillMediumSquare />
-          </a>
-          <a
-            href="https://discord.gg/42FxuQXc"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaDiscord />
-          </a>
-          <a
-            href="https://illiniblockchain.notion.site/Illini-Blockchain-Public-ac1f93f3ad984e3fa00ce6a42c69b335"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <SiNotion />
-          </a>
-          <a
-            href="https://github.com/IlliniBlockchain"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <AiFillGithub />
-          </a>
-        </FooterSocialsContainer>
+        <FooterBottomContainer>
+          <FooterSocialsContainer>
+            <a
+              href="https://twitter.com/ILL_Blockchain"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiOutlineTwitter />
+            </a>
+            <a
+              href="https://medium.com/@illiniblockchain"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillMediumSquare />
+            </a>
+            <a
+              href="https://discord.gg/42FxuQXc"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaDiscord />
+            </a>
+            <a
+              href="https://illiniblockchain.notion.site/Illini-Blockchain-Public-ac1f93f3ad984e3fa00ce6a42c69b335"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SiNotion />
+            </a>
+            <a
+              href="https://github.com/IlliniBlockchain"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillGithub />
+            </a>
+          </FooterSocialsContainer>
+
+          <FooterSponsorsContainer>
+            <h4>Sponsors</h4>
+            <FooterSponsorImageContainer>
+              <Image
+                src="/solanaLogo.png"
+                alt="Solana Logo"
+                width={350}
+                height={50}
+              />
+            </FooterSponsorImageContainer>
+          </FooterSponsorsContainer>
+        </FooterBottomContainer>
       </FooterContainer>
     </FooterSection>
   );
