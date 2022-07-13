37c6dc19155b56a38dde69e714a4ec4e12da9b16
Alec Chen <alecchendev@gmail.com>
2022-07-12 20:58:02 -0400

    Marginally fix signup button

--- components/home/NewsletterBanner/index.js
+++ components/home/NewsletterBanner/index.js
@@ -1,28 +1,32 @@
-import React from 'react'
-import { 
-  NewsletterBackground, 
-  NewsletterContainer, 
-  NewsletterHeader, 
+import React from "react";
+import {
+  NewsletterBackground,
+  NewsletterContainer,
+  NewsletterHeader,
   NewsletterSubheader,
-  NewsletterButton 
-  } from './NewsletterStyles'
-import EmailForm from '../../EmailForm'
-import Link from 'next/link'
+  NewsletterButton,
+  NewsletterFlex,
+} from "./NewsletterStyles";
+import EmailForm from "../../EmailForm";
+import Link from "next/link";
 const NewsletterBanner = () => {
-
   return (
-  <NewsletterBackground>
-    <NewsletterContainer>
-        <NewsletterHeader>Sign up for our newsletter!</NewsletterHeader>
-        <NewsletterSubheader>Receive the latest updates on everything Illini Blockchain</NewsletterSubheader>
-    </NewsletterContainer>
-    {/* TODO: Fix Netlify Forms */}
-    {/* <EmailForm/> */}
-    <Link href='https://forms.gle/TisDoiaNfD2ZoRes8' passHref>
-      <NewsletterButton>Signup</NewsletterButton>
-    </Link>
-  </NewsletterBackground>
-  )
-}
-  
-export default NewsletterBanner
+    <NewsletterBackground>
+      <NewsletterFlex>
+        <NewsletterContainer>
+          <NewsletterHeader>Sign up for our newsletter!</NewsletterHeader>
+          <NewsletterSubheader>
+            Receive the latest updates on everything Illini Blockchain
+          </NewsletterSubheader>
+        </NewsletterContainer>
+        <Link href="https://forms.gle/TisDoiaNfD2ZoRes8" passHref>
+          <NewsletterButton>Signup</NewsletterButton>
+        </Link>
+      </NewsletterFlex>
+      {/* TODO: Fix Netlify Forms */}
+      {/* <EmailForm/> */}
+    </NewsletterBackground>
+  );
+};
+
+export default NewsletterBanner;
