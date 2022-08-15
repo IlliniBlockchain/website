a3ae7b56f1ad4bc94e0dc7d77a7f6b3768aad9e9
Alec Chen <alecchendev@gmail.com>
2022-08-15 18:00:44 -0500

    Get rid of single quotes

--- components/home/ApplyBanner/index.js
+++ components/home/ApplyBanner/index.js
@@ -6,9 +6,9 @@ const ApplyBanner = () => {
   return (
   <ApplyBackground>
     <ApplyContainer>
-        <ApplyHeader>We're recruiting for fall 2022!</ApplyHeader>
+        <ApplyHeader>We are recruiting for fall 2022!</ApplyHeader>
         <ApplySubheader>
-          We're looking for developers, researchers, and designers to join us this upcoming semester!
+          We are looking for developers, researchers, and designers to join us this upcoming semester!
         </ApplySubheader>
     </ApplyContainer>
     <Link href='https://www.notion.so/illiniblockchain/Fall-2022-Recruitment-ff8e4c8165c54840bda5a77e4e95edbe' passHref>
